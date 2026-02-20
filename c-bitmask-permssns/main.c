131c87847dfc291db811d18641f19c6526af79ae
g t2 <17090087+timshaya@users.noreply.github.com>
2026-02-20 13:28:58 -0800

    initial commit

---
+++ c-bitmask-permssns/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-bitmask-permssns
+//
+//  Created by g t2 on 2/20/26.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, bitmask permissions!\n");
+    return EXIT_SUCCESS;
+}
