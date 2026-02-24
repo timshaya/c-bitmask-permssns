bef3ed0e5ec336fa210c01c0971bed608210f52a
g t2 <17090087+timshaya@users.noreply.github.com>
2026-02-24 12:22:03 -0800

    clean up

--- c-bitmask-permssns/main.c
+++ c-bitmask-permssns/main.c
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <math.h>
 
 /* Bit positions (owner / group / other: r, w, x) */
 #define PERM_OWNER_READ  ((uint16_t)1u << 8) //1u = 0000 0000 0000 0001, after (1u << 8) it becomes 0000 0001 0000 0000
@@ -20,6 +21,8 @@
 #define PERM_OTHER_READ  ((uint16_t)1u << 2)
 #define PERM_OTHER_WRITE ((uint16_t)1u << 1)
 #define PERM_OTHER_EXEC  ((uint16_t)1u << 0)
+
+char *endptr;
 
 /* Setters, etc */
 void set_perm(uint16_t *mode, uint16_t mask){
@@ -63,55 +66,57 @@ void print_mode(uint16_t mode){
 
 int main(int argc, const char * argv[]) {
     
+
     uint16_t mode = 0; //0000 0000 0000 0000 in binary
     
-    /* e.g. Set OWNER permissions to rwx------
-        
-        In binary, this is what happens set_perm(&mode, PERM_OWNER_READ | PERM_OWNER_WRITE | PERM_OWNER_EXEC) is called:
-            &mode is "value of" mode, which starts as 0 or 0000 0000 0000 0000
-                PERM_OWNER_READ  = 0000 0001 0000 0000
-                PERM_OWNER_WRITE = 0000 0000 1000 0000
-                PERM_OWNER_EXEC  = 0000 0000 0100 0000
-            then
-                0000 0000 0000 0000 // mode = 0
-                0000 0001 0000 0000 // combine with result of PERM_OWNER_READ (1u << 8)
-                ------------------- // perform a bitwise OR  (mode | mask)
-                0000 0001 0000 0000 // &mode is now 0000 0001 0000 0000 in binary, also saying READ permission is 1 or "ON"
-                
-                0000 0001 0000 0000 // mode = 0000 0001 0000 0000
-                0000 0000 1000 0000 // combine with result of PERM_OWNER_WRITE (1u << 7)
-                -------------------
-                0000 0001 1000 0000
+
+     /* e.g. Set OWNER permissions to rwx------
+         
+         In binary, this is what happens set_perm(&mode, PERM_OWNER_READ | PERM_OWNER_WRITE | PERM_OWNER_EXEC) is called:
+             &mode is "value of" mode, which starts as 0 or 0000 0000 0000 0000
+                 PERM_OWNER_READ  = 0000 0001 0000 0000
+                 PERM_OWNER_WRITE = 0000 0000 1000 0000
+                 PERM_OWNER_EXEC  = 0000 0000 0100 0000
+             then
+                 0000 0000 0000 0000 // mode = 0
+                 0000 0001 0000 0000 // combine with result of PERM_OWNER_READ (1u << 8)
+                 ------------------- // perform a bitwise OR  (mode | mask)
+                 0000 0001 0000 0000 // &mode is now 0000 0001 0000 0000 in binary, also saying READ permission is 1 or "ON"
+                 
+                 0000 0001 0000 0000 // mode = 0000 0001 0000 0000
+                 0000 0000 1000 0000 // combine with result of PERM_OWNER_WRITE (1u << 7)
+                 -------------------
+                 0000 0001 1000 0000
+      
+                 0000 0001 1000 0000
+                 0000 0000 0100 0000 // combine with result of PERM_OWNER_EXEC (1u << 6)
+                 -------------------
+                 0000 0001 1100 0000
+    */
+     set_perm(&mode, PERM_OWNER_READ | PERM_OWNER_WRITE | PERM_OWNER_EXEC);
      
-                0000 0001 1000 0000
-                0000 0000 0100 0000 // combine with result of PERM_OWNER_EXEC (1u << 6)
-                -------------------
-                0000 0001 1100 0000
-     */
-    set_perm(&mode, PERM_OWNER_READ | PERM_OWNER_WRITE | PERM_OWNER_EXEC);
+     set_perm(&mode, PERM_GROUP_READ | PERM_GROUP_EXEC);
+     
+     printf("Initial value of mode:  ");
+     print_mode(mode); //rwxr-x---
+     printf("\n");
+     
+     set_perm(&mode, PERM_GROUP_READ | PERM_GROUP_EXEC);
+     set_perm(&mode, PERM_OTHER_EXEC);
+
+     printf("2nd value of mode:  ");
+     print_mode(mode); //rwxr-x--x
+     printf("\n");
+     
+     clear_perm(&mode, PERM_GROUP_EXEC);
+     printf("Just removed PERM_GROUP_EXEC from mode, so now permissions are:\n  ");
+     print_mode(mode);
+     
+     if(has_perm(mode, PERM_GROUP_EXEC)){
+         printf("\n\nGROUP has EXEC permissios\n");
+     } else {
+         printf("\n\nGROUP does not have EXEC permissios\n\n");
+     }
     
-    set_perm(&mode, PERM_GROUP_READ | PERM_GROUP_EXEC);
-    
-    printf("Initial value of mode:  ");
-    print_mode(mode); //rwxr-x---
-    printf("\n");
-    
-    set_perm(&mode, PERM_GROUP_READ | PERM_GROUP_EXEC);
-    set_perm(&mode, PERM_OTHER_EXEC);
-  
-    printf("2nd value of mode:  ");
-    print_mode(mode); //rwxr-x--x
-    printf("\n");
-    
-    clear_perm(&mode, PERM_GROUP_EXEC);
-    printf("Just removed PERM_GROUP_EXEC from mode, so now permissions are:\n  ");
-    print_mode(mode);
-    
-    if(has_perm(mode, PERM_GROUP_EXEC)){
-        printf("\n\nGROUP has EXEC permissios\n");
-    } else {
-        printf("\n\nGROUP does not have EXEC permissios\n\n");
-    }
-        
     return EXIT_SUCCESS;
 }
